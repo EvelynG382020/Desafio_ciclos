18d6213cce21991bd5e3e430930bb9efb8834c41
evelyn gomez <evelyngomezfrez@gmail.com>
2020-10-24 22:47:37 -0300

    corrigiendo los codigos mejorando la sintaxis

---
+++ lorem_generator.rb
@@ -0,0 +1,22 @@
+
+#mostrar en pantalla
+#varios parrafos de Lorem ipsum, donde el número de párrafos 
+#se especifica al cargar el script.
+
+
+def parrafo(lorem_ipsum)
+   while lorem_ipsum >= 1
+        puts "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac lacinia nibh, nec faucibus
+        enim. Nullam quis lorem posuere, hendrerit tellus eget, tincidunt ipsum. Nam nulla tortor,
+        elementum in elit nec, fermentum dignissim sapien. Sed a mattis nisi, sit amet dignissim elit.
+        Sed finibus eros sit amet ipsum scelerisque interdum. Curabitur justo nibh, viverra a elit vel,
+        elementum hendrerit erat. Duis feugiat mattis ante vel hendrerit. Etiam nec nibh nulla. Class
+        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos"
+    lorem_ipsum = lorem_ipsum - 1
+    end
+end
+
+    lorem_ipsum = ARGV[0].to_i
+    parrafo(lorem_ipsum)
+ 
+  
