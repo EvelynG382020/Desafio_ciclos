18ee2c20b9c75409790881c88fa9219cf511888a
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-09 22:33:18 -0300

    corrijiendo el codigo

--- cuenta_regresiva.rb
+++ cuenta_regresiva.rb
@@ -7,7 +7,7 @@ cuenta_regresiva = ARGV[0].to_i
 
 #iteración
 puts "Contando desde #{cuenta_regresiva}..." 
-while cuenta_regresiva > 0
-puts cuenta_regresiva
-cuenta_regresiva -= 1 
-end
+    while cuenta_regresiva > 0
+        puts cuenta_regresiva
+        cuenta_regresiva -= 1 
+    end
--- solo_pares2.rb
+++ solo_pares2.rb
@@ -1,9 +1,8 @@
 #Crear una variante del programa anterior llamado solo_pares2.rb 
 #pero que en este el cero no sea considerado (el cero no es par)
 #ruby solo_pares2.rb 5
-n = 20
-n.times do |i|
-    if i%2 == 0 && i > 0
-        print " #{i} "        
+n = ARGV[0].to_i
+i = 0
+    while i < n * 2
+        puts i += 2        
     end    
-end
