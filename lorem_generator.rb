7e12c6662e1d95b5d4b04360bf8b09de41352350
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-19 20:36:21 -0300

    mejorando el codigo

--- lorem_generator.rb
+++ lorem_generator.rb
@@ -13,12 +13,23 @@ def parrafo(lorem_ipsum)
         elementum in elit nec, fermentum dignissim sapien. Sed a mattis nisi, sit amet dignissim elit.
         Sed finibus eros sit amet ipsum scelerisque interdum. Curabitur justo nibh, viverra a elit vel,
         elementum hendrerit erat. Duis feugiat mattis ante vel hendrerit. Etiam nec nibh nulla. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos"
+        aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\n\n"
     lorem_ipsum = lorem_ipsum - 1
     end
 end
 
     lorem_ipsum = ARGV[0].to_i
     parrafo(lorem_ipsum)
+#____________________________________
+n = ARGV[0].to_i
+
+parrafo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi ac lacinia nibh, nec faucibus
+enim. Nullam quis lorem posuere, hendrerit tellus eget, tincidunt ipsum. Nam nulla tortor,
+elementum in elit nec, fermentum dignissim sapien. Sed a mattis nisi, sit amet dignissim elit.
+Sed finibus eros sit amet ipsum scelerisque interdum. Curabitur justo nibh, viverra a elit vel,
+elementum hendrerit erat. Duis feugiat mattis ante vel hendrerit. Etiam nec nibh nulla. Class
+aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.\n\n"
  
-  
+n.times do
+    puts parrafo
+end
