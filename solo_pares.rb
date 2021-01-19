cea926294f7a76868508fee1b022ca3d472d7af0
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-19 20:05:25 -0300

    revisando codigo

--- solo_pares.rb
+++ solo_pares.rb
@@ -7,12 +7,8 @@ ruby solo_pares.rb 5
 
 n = ARGV[0].to_i 
 
-n.times do |i| #utilizo el método times para que haga el ciclo.
-    if i % 2 == 0 #La condición es que se muestren los pares, entonces los identifico usando el resto (o módulo) 
-    puts i #Si se cumple la condición, se imprime i.
-    else #Si no se cumple la condición...
-    nil #... i no se imprime
-    end
+n.times do |i|    
+    puts i*2    
 end
     
 print "\n" #salto de línea
