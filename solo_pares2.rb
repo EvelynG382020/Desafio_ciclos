b2edad4d3a324d0f5fb79c38c54e78cc2a5a5a2e
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-19 20:12:50 -0300

    modificando codigo

--- solo_pares2.rb
+++ solo_pares2.rb
@@ -4,15 +4,10 @@ pero que en este el cero no sea considerado (el cero no es par)
 ruby solo_pares2.rb 5
 =end
 
-n = ARGV[0].to_i #dato que el usuario ingresa
+n = ARGV[0].to_i 
 n.times do |i|
-    if i == 0 #si la iteración es igual a 0 
-    nil #no se imprima el 0
-    elsif i%2 == 0 #con el módulo veo si es par según su resto y se imprime i
-        puts i 
-    else 
-    nil #no se imprima el 0
-    end
+   i += 1
+   puts i*2
 end
 
 print "\n"
