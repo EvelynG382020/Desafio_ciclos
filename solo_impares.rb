5d473f47b9fbef3c69c0a1fb0c9e757be1a67d16
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-19 15:43:28 -0300

    commit

--- solo_impares.rb
+++ solo_impares.rb
@@ -11,15 +11,3 @@ n.times do |i|
         puts " #{i} "       
     end    
 end
-
-n = ARGV[0].to_i #Le pido el dato al usuario (recuerda pasarlo a integer, en este caso)
-
-n.times do |i| #utilizo el método times para que haga el ciclo.
-    if i % 2 == 1 #La condición es que se muestren los impares, entonces los identifico usando el resto (o módulo).
-    print i #Si se cumple la condición, se imprime i.
-    else #Si no se cumple la condición...
-    nil #... i no se imprime
-    end
-end
-
-print "\n" #salto de línea
--- solo_pares.rb
+++ solo_pares.rb
@@ -4,20 +4,15 @@ es ingresado por el usuario
 ruby solo_pares.rb 5
 =end
 
-n = ARGV[0].to_i
-i = -2
-    while i < (n - 1) * 2
-        puts i += 2
-    end
 
-    n = ARGV[0].to_i #Le pido el dato al usuario (recuerda pasarlo a integer, en este caso)
+n = ARGV[0].to_i 
 
-    n.times do |i| #utilizo el método times para que haga el ciclo.
-        if i % 2 == 0 #La condición es que se muestren los pares, entonces los identifico usando el resto (o módulo) 
-        print i #Si se cumple la condición, se imprime i.
-        else #Si no se cumple la condición...
-        nil #... i no se imprime
-        end
+n.times do |i| #utilizo el método times para que haga el ciclo.
+    if i % 2 == 0 #La condición es que se muestren los pares, entonces los identifico usando el resto (o módulo) 
+    puts i #Si se cumple la condición, se imprime i.
+    else #Si no se cumple la condición...
+    nil #... i no se imprime
     end
+end
     
-    print "\n" #salto de línea
+print "\n" #salto de línea
