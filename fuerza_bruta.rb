cdb5f2a666f01d733afa06ff92f30006f47df51e
evelyn gomez <evelyngomezfrez@gmail.com>
2021-01-19 21:51:11 -0300

    mejorando el codigo

--- fuerza_bruta.rb
+++ fuerza_bruta.rb
@@ -10,11 +10,11 @@ luego con z, luego ab, ac, .. az, aba ... azz ... zzz, aaaa
 #ruby fuerza_bruta.rb passwo 190906392 intentos
 
 password = ARGV[0]
-letra = "a"
-suma = 0
+letra_inicio= "a"
+pass_fallidos = 0
 
-while letra != password
-    letra = letra.next
-    suma += 1
+while letra_inicio != password
+    letra_inicio = letra_inicio.next
+    pass_fallidos += 1
 end
-puts suma
+puts "#{pass_fallidos} intentos"
